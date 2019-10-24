6d953eb70b2c886a493cea82aaf96e64ff4dcd48
X <evilbunny.x@gmail.com>
2019-10-24 15:02:10 +0300

    complete tool

--- stanrate/main.go
+++ stanrate/main.go
@@ -1,51 +1,97 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
+	"os"
+	"os/signal"
+	"strings"
 	"sync/atomic"
 	"time"
 
 	"github.com/nats-io/stan.go"
 )
 
-var subjects = map[string]*uint64{}
-var subjectOrder = []string{}
+const (
+	defaultClusterID      = "nats"
+	defaultClientIDPrefix = "stanrate"
+)
+
+var (
+	subjects     = map[string]*uint64{}
+	subjectOrder = []string{}
+)
 
 func main() {
-	clientID := fmt.Sprintf("stanrate-%d", rand.Int())
-	sc, err := stan.Connect(clusterID, clientID)
+	var (
+		subjects, clusterID, clientID, natsUrl string
+		interval                               int
+		help                                   bool
+	)
+
+	flag.StringVar(&subjects, "subject", "", "Comma separated list of subjects")
+	flag.StringVar(&clusterID, "cluster", defaultClusterID, "Cluster ID")
+	flag.StringVar(&clientID, "client_id", fmt.Sprintf("%s-%d", defaultClientIDPrefix, rand.Intn(1000)), "Client ID")
+	flag.StringVar(&natsUrl, "url", "nats://client:123456@localhost:4222", "NATS server url")
+	flag.IntVar(&interval, "int", 5, "Interval in seconds between checks")
+	flag.BoolVar(&help, "help", false, "This help text")
+
+	flag.Parse()
+	if help {
+		flag.Usage()
+	}
+
+	sc, err := stan.Connect(clusterID, clientID, stan.NatsURL(natsUrl))
 	if err != nil {
 		log.Fatalf("can not connect to NATS server: %v", err)
 	}
 	defer sc.Close()
 
-	// Unsubscribe
-	sub, err := countRate("order", sc)
-	if err != nil {
-		log.Println("can not subscribe to %q: %v", "order", err)
-		return
-	}
-	defer sub.Unsubscribe()
+	// monitor each subject
+	for _, s := range strings.Split(subjects, ",") {
+		subject := strings.TrimSpace(s)
 
-	go monitor(5)
+		sub, err := countRate(subject, sc)
+		if err != nil {
+			log.Printf("can not subscribe to %q: %v", subject, err)
+			return
+		}
+
+		defer func() {
+			log.Println("unsubscribe", subject)
+			err := sub.Unsubscribe()
+			if err != nil {
+				log.Printf("can not unsubscribe %q: %v", subject, err)
+			}
+		}()
+	}
+
+	go monitor(interval)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt)
+	<-done
+
+	log.Println("exit")
 
 	// TODO: wait for Ctrl+C
 }
 
 func countRate(subj string, sc stan.Conn) (stan.Subscription, error) {
-	var counter *uint64
-	*counter = 0
-	subjects[subj] = counter
+	var counter uint64
+	subjects[subj] = &counter
 	subjectOrder = append(subjectOrder, subj)
 
 	return sc.Subscribe(subj, func(m *stan.Msg) {
-		atomic.AddUint64(counter, 1)
+		atomic.AddUint64(&counter, 1)
 	})
 }
 
 func monitor(intervalSec int) {
+	log.Printf("monitor %s every %d sec", strings.Join(subjectOrder, ", "), intervalSec)
+
 	ticker := time.Tick(time.Second * time.Duration(intervalSec))
 
 	for range ticker {
